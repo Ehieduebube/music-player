3b5cec7a48580bbd18875546f87ada59211fd628
Ebube <139717141+Ehieduebube@users.noreply.github.com>
2023-12-14 07:57:20 -0800

    Add files via upload

---
+++ src/Data.js
@@ -0,0 +1,80 @@
+// FileName: data.js 
+
+ 
+function chillHop() { 
+return [ 
+	{ 
+	name: "Cast ft ODOMODUBLVCK", 
+	cover: 
+"https://www.naijaloaded.com.ng/wp-content/uploads/2023/11/Shallipopi.jpeg", 
+	artist: " Shallipopi", 
+	audio: 
+"https://naijaloaded.store/assets/uploads/Shallipopi-Ft.-Odumodublvck-Cast.mp3", 
+	color: ["#205950", "#2ab3bf"], 
+	
+	active: true, 
+	}, 
+
+	{ 
+	name: "A1 PERICO (feat. Nasty C & Mizzle)", 
+	cover: 
+		"https://i0.wp.com/justnaija.com/uploads/2023/12/Odumodublvck-Eziokwu-Uncut-Deluxe-Album-artwork?ulb=false&ssl=1&resize=320,350", 
+	artist: "ODUMODUBLVCK", 
+	audio: 
+		"https://cdn3.justnaija.me/uploads/music/2023/12/ODUMODUBLVCK-A1-PERICO-Ft-Nasty-C-Mizzle-(JustNaija.com).mp3", 
+	color: ["#EF8EA9", "#ab417f"], 
+	
+	active: false, 
+	}, 
+
+	{ 
+	name: "SAINT OBI Ft Reeplay", 
+	cover: 
+	"https://i0.wp.com/justnaija.com/uploads/2023/12/Odumodublvck-Eziokwu-Uncut-Deluxe-Album-artwork?ulb=false&ssl=1&resize=320,350", 
+	artist: " ODUMODUBLVCK", 
+	audio: 
+"https://cdn3.justnaija.me/uploads/music/2023/10/ODUMODUBLVCK-SAINT-OBI-Ft-Reeplay-(JustNaija.com).mp3", 
+	color: ["#CD607D", "#c94043"], 
+	
+	active: false, 
+	}, 
+
+	{ 
+	name: "Plugged In Ft. ODUMODUBLVCK", 
+	cover: 
+"https://www20.24naijamuzic.com/wp-content/uploads/2023/12/Fumez-The-Engineer-ODUMODUBLVCK-Plugged-In.jpg", 
+	artist: "Fumez The Engineer", 
+	audio: 
+"https://vineloaded.com.ng/wp-content/uploads/2023/12/Fumez_The_Engineer_ODUMODUBLVCK_-_Plugged_In_vineloaded.com.ng.mp3", 
+	color: ["#EF8EA9", "#ab417f"], 
+	
+	active: false, 
+	}, 
+
+	{ 
+	name: "Naija Shawty ft Victony ", 
+	cover: 
+"https://i0.wp.com/xclusiveloaded.com/wp-content/uploads/2023/10/Blaqbonez-%E2%80%93-Emeka-Must-Shine-EP.webp?fit=544%2C544&ssl=1", 
+	artist: "Blaqbonez", 
+	audio: 
+"https://cdn.trendybeatz.com/audio/Blaqbonez-Ft-Victony-Naija-Shawty-(TrendyBeatz.com).mp3", 
+	color: ["#CD607D", "#c94043"], 
+	
+	active: false, 
+	}, 
+
+	{ 
+	name: "Dollarz Ft. ODUMODUBLVCK", 
+	cover: 
+"https://i0.wp.com/xclusiveloaded.com/wp-content/uploads/2023/10/Blaqbonez-%E2%80%93-Emeka-Must-Shine-EP.webp?fit=544%2C544&ssl=1", 
+	artist: "Blaqbonez", 
+	audio: 
+"https://cdn.xclusiveloaded.com/wp-content/uploads/2023/10/Blaqbonez_Ft_Odumodublvck_-_Dollerz.mp3", 
+	color: ["#205950", "#2ab3bf"], 
+	
+	active: false, 
+	}, 
+]; 
+} 
+
+export default chillHop; 
